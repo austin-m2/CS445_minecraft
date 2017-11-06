eeb6abfa23f677253a65045afcbbcf2ead078991
austin-m2 <morris.austinm@gmail.com>
2017-11-05 19:20:53 -0800

    Implemented FPCameraController class
    I put in the FPCameraController class using the code from the slides.

--- src/minecraft/Main.java
+++ src/minecraft/Main.java
@@ -10,33 +10,34 @@
 *
 ****************************************************************/ 
 package minecraft;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.*;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.util.glu.GLU;
 
 /**
  *
  * @author morri
  */
 public class Main {
-
-    public static void main(String[] args) throws FileNotFoundException {        
+    private FPCameraController fp = new FPCameraController(0f, 0f, 0f);
+    private DisplayMode displayMode;
+    
+    public static void main(String[] args) {        
         Main main = new Main();
         main.start();
     }
 
-    public Main() {
-    }
+    //public Main() {
+    //}
     
     public void start() {
         try {
             createWindow();
             initGL();
-            render();
+            fp.gameLoop();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -44,8 +45,17 @@ public class Main {
         
     private void createWindow() throws Exception {
         Display.setFullscreen(false);
-        Display.setDisplayMode(new DisplayMode(640, 480));
-        Display.setTitle("Oh Yeah!");
+        DisplayMode d[] = Display.getAvailableDisplayModes();
+        for (int i = 0; i < d.length; i++) {
+            if (d[i].getWidth() == 640
+                    && d[i].getHeight() == 480
+                    && d[i].getBitsPerPixel() == 32) {
+                displayMode = d[i];
+                break;
+            } 
+        }
+        Display.setDisplayMode(displayMode);
+        Display.setTitle("Minecraft Too");
         Display.create();
     }
     
@@ -53,32 +63,14 @@ public class Main {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        glOrtho(-320, 320, -240, 240, 1, -1);
+        GLU.gluPerspective(100.0f, (float)displayMode.getWidth() / (float)displayMode.getHeight(), 0.1f, 300.0f);
         glMatrixMode(GL_MODELVIEW);
         glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
     }
     
-    private void render() {
-        while (!Display.isCloseRequested()) {
-            try {
-                if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
-                    System.exit(0);
-                }
 
-                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-                glLoadIdentity();
-                
-                glColor3f(1.0f, 1.0f, 0.0f);
-                glPointSize(1);
-                
-                
-                Display.update();
-                Display.sync(60);
-            } catch (Exception e) {}
-        }
-        Display.destroy();
-    }
     
+
 }
 
 
